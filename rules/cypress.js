cc3b77f9e822c351e8f7483a77164e4f67dd76bb
Badisi <badisi84@gmail.com>
2021-04-21 18:26:12 +0200

    fix(rules): less stricter rules for cypress

--- rules/cypress.js
+++ rules/cypress.js
@@ -4,11 +4,28 @@ module.exports = {
     },
     "plugins": [
         "cypress",
-        "chai-friendly"
+        "chai-friendly",
+        "@typescript-eslint"
     ],
     "rules": {
-        // Disallow Unused Variables
+        // Disallow unused variables
         // https://eslint.org/docs/rules/no-unused-vars
-        "no-unused-vars": "off"
+        "no-unused-vars": "off",
+
+        // Disallow assigning any to variables and properties
+        // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-unsafe-assignment.md
+        "@typescript-eslint/no-unsafe-assignment": "off",
+
+        // Disallow calling an any type value
+        // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-unsafe-call.md
+        "@typescript-eslint/no-unsafe-call": "off",
+
+        // Disallow member access on any typed variables
+        // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-unsafe-member-access.md
+        "@typescript-eslint/no-unsafe-member-access": "off",
+
+        // Enforce template literal expressions to be of string type
+        // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/restrict-template-expressions.md
+        "@typescript-eslint/restrict-template-expressions": "off"
     }
 };
