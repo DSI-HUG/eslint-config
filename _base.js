755d046b57a80e0456628e26d2c09e996e320d0a
Badisi <badisi84@gmail.com>
2022-05-02 23:40:20 +0200

    chore: improve licensing

--- _base.js
+++ _base.js
@@ -1,3 +1,10 @@
+/**
+ * @license
+ * @hug/eslint-config <https://github.com/DSI-HUG/eslint-config>
+ * Released under GNU General Public License v3.0 only <https://github.com/DSI-HUG/eslint-config/blob/main/LICENSE>
+ * SPDX-License-Identifier: GPL-3.0-only
+ * Copyright (C) 2022 HUG
+ */
 'use strict';
 
 // This is a workaround for vscode not finding tsconfig.eslint.json when a workspace is opened
