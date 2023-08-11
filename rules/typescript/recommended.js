29033fd03f49d16e3a9d8721ca9473d90136fe54
Badisi <badisi84@gmail.com>
2023-08-11 12:49:30 +0200

    chore(deps)!: upgrade to typescript-eslint 6.x

--- rules/typescript/recommended.js
+++ rules/typescript/recommended.js
@@ -5,8 +5,8 @@ module.exports = {
         "@typescript-eslint"
     ],
     "extends": [
-        "plugin:@typescript-eslint/recommended",
-        "plugin:@typescript-eslint/recommended-requiring-type-checking"
+        "plugin:@typescript-eslint/recommended-type-checked",
+        "plugin:@typescript-eslint/stylistic-type-checked"
     ],
     "rules": {
         "max-len": "off",
